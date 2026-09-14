d0ca5b0a4da1be269f26e6afd1434e73e1c3945f
amctech106 <121874402+amctech106@users.noreply.github.com>
2026-09-14 21:55:51 +0500

    final

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const takeInput = document.querySelector("#take-input");
 const checkBtn = document.querySelector("#check");
 const refreshBtn = document.querySelector("#reset");
 
-// پیراگرافس کو سلیکٹ کیا ہے
+//ٹیبل کا پہلے کالمز سلیکٹ کیا
+
+const label2 = document.querySelector("#label2");
+const label3 = document.querySelector("#label3");
+const label4 = document.querySelector("#label4");
+const label5 = document.querySelector("#label5");
+const label6 = document.querySelector("#label6");
+const label7 = document.querySelector("#label7");
+const label8 = document.querySelector("#label8");
+
+// ٹیبل کے دوسرے کالمز کو سلیکٹ کیا
 
 const para1 = document.querySelector("#para1");
 const para2 = document.querySelector("#para2");
@@ -14,8 +24,6 @@ const para6 = document.querySelector("#para6");
 const para7 = document.querySelector("#para7");
 const para8 = document.querySelector("#para8");
 
-
-
 // چیک بٹن کلک کرنے پر یہ فنکشن چلے گا ۔
 
 checkBtn.addEventListener("click", async function (event) {
@@ -23,34 +31,35 @@ checkBtn.addEventListener("click", async function (event) {
 
   const city = takeInput.value.trim();
 
+
   const result = await axios(
     `https://api.weatherapi.com/v1/current.json?key=60e0a3d2f152486e950213038260606&q=${city}`,
   );
 
+
   takeInput.value = "";
 
-    para1.innerHTML = `<i class="fa-solid fa-cloud"></i>`;
 
-  document.querySelector("#label2").innerHTML = "Temp";
+  para1.innerHTML = `<i class="fa-solid fa-cloud"></i>`;
+
+  label2.innerHTML = "Temp";
   para2.innerHTML = `${Math.floor(result.data.current.temp_c)}°C `;
 
-  document.querySelector("#label3").innerHTML = "Feeling";
+  label3.innerHTML = "Feeling";
   para3.innerHTML = `${Math.floor(result.data.current.feelslike_c)}`;
 
-  document.querySelector("#label4").innerHTML = "Humidity";
+  label4.innerHTML = "Humidity";
   para4.innerHTML = `${result.data.current.humidity}`;
 
-  document.querySelector("#label5").innerHTML = "Wind Kph";
+  label5.innerHTML = "Wind Kph";
   para5.innerHTML = `${result.data.current.wind_kph}`;
 
-  document.querySelector("#label6").innerHTML = "Country";
+  label6.innerHTML = "Country";
   para6.innerHTML = `${result.data.location.country}`;
 
-  document.querySelector("#label7").innerHTML = "Province";
+  label7.innerHTML = "Province";
   para7.innerHTML = `${result.data.location.region}`;
 
-  document.querySelector("#label8").innerHTML = "City";
-  para8.innerHTML = `${result.data.location.name}`;;
+  label8.innerHTML = "City";
+  para8.innerHTML = `${result.data.location.name}`;
 });
-
-
