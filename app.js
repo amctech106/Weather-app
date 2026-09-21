51cd92fdec624999fc47515d09d6d79a79131c91
Abdul Majeed <121874402+amctech106@users.noreply.github.com>
2026-09-21 17:54:04 +0500

    Reset image source on error handling
    Clear the image source on error instead of displaying it.

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ checkBtn.addEventListener("click", async function (event) {
   } catch (error) {
     para1.innerHTML = error.response.data.error.message;
 
-    para0.src = `${result.data.current.condition.icon}`;
+    para0.src = ``;
     label2.innerHTML = "";
     para2.innerHTML = "";
 
